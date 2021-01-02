193870cefbd376933e1be8e41102f27603e1ae0a
manhph2211 <maxph22112000@gmail.com>
2021-01-02 20:12:05 +0700

    add some

--- get_data.py
+++
@@ -1,6 +0,0 @@
-from google_images_download import google_images_download  #importing the library 
-
-response = google_images_download.googleimagesdownload()  #class instantiation 
-arguments = {"keywords":"bien so xe o to","limit":10,"print_urls":True}  #creating list of arguments 
-paths = response.download(arguments)  #passing the arguments to the function 
-print(paths)
