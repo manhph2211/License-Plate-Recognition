6c3749fedd193d8735e5be99247629c8b27905d0
manhph2211 <maxph22112000@gmail.com>
2021-01-04 11:18:23 +0700

    successfully got letters

--- segementing_Letter_Using_CV2.py
+++ segementing_Letter_Using_CV2.py
@@ -30,8 +30,9 @@ def DiffImage(LpImg):
 	
 ## test DiffImage
 
-'''
+
 ### load model
+'''
 wpod_net_path = "wpod-net.json"
 wpod_net = load_model(wpod_net_path)
 
@@ -58,3 +59,60 @@ for i in range(len(plot_image)):
         plt.imshow(plot_image[i],cmap="gray")
 plt.show()
 '''
+
+
+# Create sort_contours() function to grab the contour of each digit from left to right
+def sort_contours(cnts,reverse = False):
+    i = 0
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+    return cnts
+
+def get_Crop_Letter(wpod_net,test_image_path):
+
+	vehicle, LpImg,cor = get_plate(wpod_net,test_image_path)
+	plate_image,gray,blur,binary,thre_mor=DiffImage(LpImg)
+	cont, _  = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	# creat a copy version "test_roi" of plat_image to draw bounding box
+	test_roi = plate_image.copy()
+
+	# Initialize a list which will be used to append charater image
+	crop_characters = []
+
+	# define standard width and height of character
+	digit_w, digit_h = 30, 60
+
+	for c in sort_contours(cont):
+	    (x, y, w, h) = cv2.boundingRect(c)
+	    ratio = h/w
+	    if 1<=ratio<=3.5: # Only select contour with defined ratio
+	        if h/plate_image.shape[0]>=0.5: # Select contour which has the height larger than 50% of the plate
+	            # Draw bounding box arroung digit number
+	            cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255,0), 2)
+
+	            # Sperate number and gibe prediction
+	            curr_num = thre_mor[y:y+h,x:x+w]
+	            curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+	            _, curr_num = cv2.threshold(curr_num, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	            crop_characters.append(curr_num)
+
+	return test_roi,crop_characters
+	
+## test getCropLetters
+
+### load model
+wpod_net_path = "wpod-net.json"
+wpod_net = load_model(wpod_net_path)
+
+
+### image test
+test_image_path = "Plate_examples/germany_car_plate.jpg"
+test_roi,crop_characters=get_Crop_Letter(wpod_net,test_image_path)
+
+print("Detect {} letters...".format(len(crop_characters)))
+fig = plt.figure(figsize=(10,6))
+plt.axis(False)
+plt.imshow(test_roi)
+plt.show()
